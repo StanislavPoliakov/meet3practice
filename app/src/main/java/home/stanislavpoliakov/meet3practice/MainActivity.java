9561799636cd2c588493c1559a8a90429206b4b9
Stanislav Poliakov <Stanislav.Poliakov@gmail.com>
2018-12-05 16:35:14 +0300

    Готов функционал отправки сообщений сервисом Полученные сообщения корректно отображаются в Activity

--- app/src/main/java/home/stanislavpoliakov/meet3practice/MainActivity.java
+++ app/src/main/java/home/stanislavpoliakov/meet3practice/MainActivity.java
@@ -1,5 +1,7 @@
 package home.stanislavpoliakov.meet3practice;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,7 +17,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i(LOG_TAG, "Main Activity: Created");
+        //Log.i(LOG_TAG, "Main Activity: Created");
         initButtons();
     }
 
@@ -28,29 +30,42 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Button secondButton = findViewById(R.id.second_button);
         firstButton.setOnClickListener(this);
         secondButton.setOnClickListener(this);
-        Log.d(LOG_TAG, "Main Activity: Initialization successful");
+        //Log.d(LOG_TAG, "Main Activity: Initialization successful");
     }
 
+    /**
+     * Метод обработки нажатий кнопок.
+     * Верхняя (первая) кнопка принудительно запускает и останавливает сервис.
+     * Нижняя (вторая) кнопка открывает вторую Activity
+     * @param v
+     * Мы передаем значение null в качестве параметра метода запроса на Intent от сервиса, потому
+     * что не обрабатываем на этой Activity сообщения сервиса, но обрабатываем их на второй Activity
+     */
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.first_button:
                 if (!isServiceStarted) {
                     //Запуск сервиса по onStartCommand
-                    startService(MyService.newIntent(MainActivity.this));
+                    startService(MyService.newIntent(MainActivity.this, null));
                     isServiceStarted = true;
-                    firstButton.setText("Остановить сервис");
+                    firstButton.setText(R.string.stop_service);
                 } else {
                     //Останавливаем сервис
-                    stopService(MyService.newIntent(MainActivity.this));
-                    Log.d(LOG_TAG, "Main Activity: Stopping Service...");
+                    stopService(MyService.newIntent(MainActivity.this, null));
+                    MyService.isStarted = false;
+                    //Log.d(LOG_TAG, "Main Activity: Stopping Service...");
                     isServiceStarted = false;
-                    firstButton.setText("Запустить сервис");
+                    firstButton.setText(R.string.start_service);
                 }
                 break;
             case R.id.second_button:
                 startActivity(SecondActivity.newIntent(MainActivity.this));
                 break;
         }
+    }
+
+    public static Intent newIntent(Context context) {
+        return new Intent(context, MainActivity.class);
     }
 }
--- app/src/main/java/home/stanislavpoliakov/meet3practice/SecondActivity.java
+++ app/src/main/java/home/stanislavpoliakov/meet3practice/SecondActivity.java
@@ -4,7 +4,10 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,19 +17,64 @@ import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity implements View.OnClickListener {
     final String LOG_TAG = "meet3_logs";
-    private TextView messageView;
+    private TextView messageView, serviceStatusView;
     private Button unbindService_button;
+    static boolean isActive;
+
+    /* Определяем приёмник сообщений и вешаем на него обработчик входящих сообщений IncomingHandler, в котором
+       мы переодпределим метод handleMessage для того, чтобы правильно обрабатывать те сообщений, которые к нам приходят.
+       Примечательно то, что нам нет необходимости определять источник этих сообщений!!! Мы, как я понимаю,
+       обрабатываем все, что к нам может прийти. То есть мы не создаем пару источник-приемник, а вешаем
+       обработчик входящих сообщений, из какого бы источник они не пришли. */
+    final Messenger mMessenger = new Messenger(new IncomingHandler());
+
+    /**
+     * Это внутренний класс обработки входящих сообщений. Отправитель этих сообщений - Service.
+     * Мы парсим входящее сообщение, чтобы узнать текущее состояние сервиса и вывести для пользователя
+     * результаты в TextView
+     */
+    class IncomingHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+
+                // STARTED и UNBINDED - результат работы сервиса, который запущен принудетельно,
+                    //но отвязан от второй Activity
+                case MyService.MSG_SERVICE_STARTEDUNBINDED:
+                    serviceStatusView.setText(R.string.started_unbinded);
+                    messageView.setText(msg.getData().getString("result"));
+                    break;
+
+                    // STARTED и BINDED - результат работы сервиса, который запущен принудительно,
+                    // и имеет привязку ко второй Activity
+                case MyService.MSG_SERVICE_STARTEDBINDED:
+                    serviceStatusView.setText(R.string.started_binded);
+                    messageView.setText(msg.getData().getString("result"));
+                    break;
+
+                    // ONLY BINDED - результат работы сервиса, который не запускался (или был остановлен)
+                    // принудительно, но сохраняет привязку ко второй Activity
+                case MyService.MSG_SERVICE_ONLYBINDED:
+                    serviceStatusView.setText(R.string.only_binded);
+                    messageView.setText(msg.getData().getString("result"));
+                    break;
+                default:
+                    super.handleMessage(msg);
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-        Log.i(LOG_TAG, "Second Activity: Created");
+        //Log.i(LOG_TAG, "Second Activity: Created");
         initItems();
 
-        //Привязываем сервис в момент создания Activity
-        bindService(MyService.newIntent(SecondActivity.this), serviceConnection, BIND_AUTO_CREATE);
-        Log.d(LOG_TAG, "Second Activity: Binding Service...");
+        //Привязываем сервис в момент создания Activity. Текущий приёмник сообщений (mMessenger)
+        //передаем в сервис, чтобы он знал, кому слать сообщения!
+        bindService(MyService.newIntent(SecondActivity.this, mMessenger), serviceConnection, BIND_AUTO_CREATE);
+        //Log.d(LOG_TAG, "Second Activity: Binding Service...");
     }
 
     private ServiceConnection serviceConnection = new ServiceConnection() {
@@ -46,6 +94,7 @@ public class SecondActivity extends AppCompatActivity implements View.OnClickLis
      */
     private void initItems() {
         messageView = findViewById(R.id.messageView);
+        serviceStatusView = findViewById(R.id.serviceStatusView);
         unbindService_button = findViewById(R.id.unbind_button);
         unbindService_button.setOnClickListener(this);
         unbindService_button.setEnabled(true);
@@ -55,7 +104,19 @@ public class SecondActivity extends AppCompatActivity implements View.OnClickLis
     public void onClick(View v) {
         unbindService(serviceConnection);
         unbindService_button.setEnabled(false);
-        Log.d(LOG_TAG, "Second Activity: Unbinding Service...");
+        //Log.d(LOG_TAG, "Second Activity: Unbinding Service...");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        isActive = true;
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        isActive = false;
     }
 
     /**
